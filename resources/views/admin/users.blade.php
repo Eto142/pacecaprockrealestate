d5b84ae58fe37f8ba107b6ac6eff24380dcc9ef2
eto <eto.saviour80@gmail.com>
2026-08-15 16:46:18 +0100

    first commit

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,43 +1,34 @@
 @extends('layouts.admin')
 @section('content')
-<div class="main-panel">
-        <div class="content-wrapper">
-          <div class="row">
-<div class="col-lg-12 stretch-card">
-              <div class="card">
-                <div class="card-body">
-                  <h4 class="card-title">Users</h4>
-                  <div class="table-responsive pt-3">
-                    <table class="table table-bordered">
-                      <thead>
-                        <tr>
-                          <th>
-                            Full Name
-                          </th>
-                          <th>
-                            Email
-                          </th>
-                          <th>
-                            Action
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
-
-                      @foreach($result  as $users)
-                      <tr class="table-primary">
-                             <td>{{$users->name}}</td>
-                             <td>{{$users->email}}</td>
-                      <td>
-                    <a type="submit" class="btn btn-danger" href="{{url('profile/'.$users->id)}}">View</a>
-                        </td>
-                     </tr>
-                    @endforeach
-                    </table>
-                  </div>
-                </div>
-              </div>
-            </div>
-</div>
+<div class="row">
+  <div class="col-12 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Users</h4>
+        <div class="table-responsive pt-3">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>Full Name</th>
+                <th>Email</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($result as $users)
+              <tr class="table-primary">
+                <td>{{ $users->name }}</td>
+                <td>{{ $users->email }}</td>
+                <td>
+                  <a class="btn btn-danger btn-sm" href="{{ url('profile/'.$users->id) }}">View</a>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
